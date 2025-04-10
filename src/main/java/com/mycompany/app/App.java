36095d089c06a24e183f045b35c3da2fa58332d7
tyhean <tyhean@gmail.com>
2025-04-10 14:52:21 +0800

    Moified for some changes !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I really upset wit jenkns!!!");
+        System.out.println("I really feel FUN with jenkns!!!");
     }
 
     public String getMessage() {
