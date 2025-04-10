b58e348a9de01dccc8aca7345daaeb3c1c8166a3
tyhean <tyhean@gmail.com>
2025-04-10 08:15:25 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I am here to learn Jenkins with Victor Tan");
+        System.out.println("Modified from locl project!!!");
     }
 
     public String getMessage() {
