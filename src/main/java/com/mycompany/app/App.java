d481df8b1e46022104555f5d6e5ed2e89a9bb781
tyhean <tyhean@gmail.com>
2025-04-10 08:37:50 +0800

    Corrected my stupid error !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I HATE Jenkins";
+    private static final String MESSAGE = "I Love Jenkins";
 
     public App() {}
 
