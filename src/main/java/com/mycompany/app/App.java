1e305beba35468222febe54503b78dfaa5daf4db
tyhean <tyhean@gmail.com>
2025-04-10 14:15:05 +0800

    Modified for pipeline project !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project is so good!!!");
+        System.out.println("I really upset wit jenkns!!!");
     }
 
     public String getMessage() {
