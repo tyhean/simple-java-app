44cc7173d96a5d184e1e3663cdaf27191d8beeb7
tyhean <tyhean@gmail.com>
2025-04-10 09:50:33 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is so cool in period!!!");
+        System.out.println("Modified from local project is so good!!!");
     }
 
     public String getMessage() {
